9d11fb5bade3e1cb78736299665e652600c49c5d
QL00036 <linshengxing@outlook.com>
2019-08-15 17:11:57 +0800

    添加定时器更新显示诗词

--- controllers/addPoetry.js
+++ controllers/addPoetry.js
@@ -11,14 +11,41 @@ module.exports = {
         var 
             title = ctx.request.body.title || '',
             author = ctx.request.body.author || '',
-            content = ctx.request.body.content || '';
+            content = ctx.request.body.content || '',
+            message;
         title = '《' + title + '》';
-        await Poetry.create({
-            title: title,
-            author: author,
-            content: content
-        }).then(function (result) {
-            // console.log('添加成功！');
+        await Poetry.findAll({
+            where: {
+                title: title,
+                author: author,
+                content: content
+            }
+        }).then(function (poetrys) {
+            if (poetrys.length > 0) {
+                message = '已存在该诗词，您可以继续添加其他诗词！';
+                ctx.render('addpoetryresult.html', {
+                    title: 'result',
+                    message: message,
+                });
+            } else {
+                Poetry.create({
+                    title: title,
+                    author: author,
+                    content: content
+                }).then(result => {
+                    if (result.id != '' && result.id != undefined) {
+                        message = '成功添加诗词，感谢您的贡献！';
+                    } else {
+                        message = '添加新诗词出错了。。。';
+                    }
+                    ctx.render('addpoetryresult.html', {
+                        title: 'result',
+                        message: message,
+                    });
+                });
+            }
+            // ctx.response.body = message;
+            // ctx.response.type = 'text/html';
         });
     }
 }
--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,13 @@
 const model = require('../model');
+const schedule = require('node-schedule');
+
+// 每次项目启动则重置，用于显示诗文
+var showIndex = 0;
+
+// 设置定时器，每隔2小时显示下一首诗词
+schedule.scheduleJob('*/2 * * *', function () {
+    showIndex++;
+});
 
 module.exports = {
     'GET /': async (ctx, next) => {
@@ -8,12 +17,14 @@ module.exports = {
                 ['createdAt', 'DESC']
             ]
         }).then(function (poetrys) {
+            // 索引超过诗文的总数则重置，再显示诗文
+            if (showIndex > poetrys.length) {
+                showIndex = 0;
+            }
             ctx.render('index.html', {
                 title: 'Welcome',
-                poetry: poetrys[0]
+                poetry: poetrys[showIndex]
             });
         });
-
-
     }
 }

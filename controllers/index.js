de1f16d4ab4a0e91f3ccefa2475093c1732939bd
LinW <13328416934@163.com>
2020-03-26 20:02:32 +0800

    调整

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,21 @@
-// const schedule = require('node-schedule');
-const https = require('https');
+const model = require('../model');
 
 module.exports = {
     'GET /': async (ctx, next) => {
-        ctx.render('oneword.html', {
-            title: '一句古诗词',
+        let Poetry = model.Poetry;
+        await Poetry.findAll({
+            order: [
+                ['createdAt', 'ASC']
+            ]
+        }).then(function (poetrys) {
+            // 索引超过诗文的总数则重置，再显示诗文
+            var showIndex = Math.floor(Math.random() * poetrys.length);
+
+            ctx.render('index.html', {
+                title: '诗词杂货铺',
+                poetry: poetrys[showIndex],
+                total: poetrys.length
+            });
         });
     }
 }
--- controllers/oneword.js
+++
@@ -1,9 +0,0 @@
-const https = require('https');
-
-module.exports = {
-    'GET /oneword': async(ctx, next) => {
-    ctx.render('oneword.html', {
-        title: '一句古诗词',
-        });
-    }
-}

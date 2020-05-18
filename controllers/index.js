5f1dedd2f143fdf757025ad42aad3a0dbfc4b327
Lins <linshengxing30@gmail.com>
2020-05-18 23:41:40 +0800

    下载功能

--- controllers/index.js
+++ controllers/index.js
@@ -12,20 +12,24 @@ module.exports = {
         }).then(function (poetrys) {
             // 索引超过诗文的总数则重置，再显示诗文
             var showIndex = Math.floor(Math.random() * poetrys.length);
-            var content = poetrys[showIndex].content;
-            gm('./static/img/background1.png')
-                .font('./static/fonts/chinese.stxingka.ttf')
-                .fontSize(40)
-                .drawText(0, 0, content, 'center')
-                .write('./static/img/draw.jpg', function (err) {
-                    console.log(err);
-                });
             ctx.render('index.html', {
                 title: '诗词杂货铺',
                 poetry: poetrys[showIndex],
                 total: poetrys.length
             });
         });
+    },
+    'POST /draw': async (ctx, next) => {
+        var content = ctx.request.body;
+        gm('./static/img/background1.png')
+            .font('./static/fonts/chinese.stxingka.ttf')
+            .fontSize(40)
+            .drawText(0, 0, content, 'center')
+            .write('./static/img/draw.jpg', function (err) {
+                if (err)
+                    console.log(err);
+                ctx.body = '{success: true}';
+            });
     },
     'GET /download': async (ctx, next) => {
         let imgPath = './static/img/draw.jpg';

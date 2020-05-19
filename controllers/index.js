f81f98f948a276aa54d01415b9c98649ab9b7970
LinW <13328416934@163.com>
2020-05-19 23:45:26 +0800

    修改图片生成

--- controllers/index.js
+++ controllers/index.js
@@ -5,26 +5,21 @@ const send = require('koa-send');
 module.exports = {
     'GET /': async (ctx, next) => {
         let Poetry = model.Poetry;
+        var poetrys;
         await Poetry.findAll({
             order: [
                 ['createdAt', 'ASC']
             ]
-        }).then(function (poetrys) {
-            // 索引超过诗文的总数则重置，再显示诗文
-            var showIndex = Math.floor(Math.random() * poetrys.length);
-            var content = poetrys[showIndex].content;
-            gm('./static/img/background1.png')
-                .font('./static/fonts/chinese.stxingka.ttf')
-                .fontSize(40)
-                .drawText(0, 0, content, 'center')
-                .write('./static/img/draw.jpg', function (err) {
-                    console.log(err);
-                });
-            ctx.render('index.html', {
-                title: '诗词杂货铺',
-                poetry: poetrys[showIndex],
-                total: poetrys.length
-            });
+        }).then(function (data) {
+            poetrys = data;
+        });
+        var showIndex = Math.floor(Math.random() * poetrys.length);
+        var content = poetrys[showIndex].content;
+        await drawImg(content);
+        ctx.render('index.html', {
+            title: '诗词杂货铺',
+            poetry: poetrys[showIndex],
+            total: poetrys.length
         });
     },
     'GET /download': async (ctx, next) => {
@@ -32,4 +27,20 @@ module.exports = {
         ctx.attachment(imgPath);
         await send(ctx, imgPath);
     }
+}
+
+async function drawImg(content) {
+    await new Promise((resolve, reject) => {
+        gm('./static/img/background1.png')
+            .font('./static/fonts/chinese.stxingka.ttf')
+            .fontSize(40)
+            .drawText(0, 0, content, 'center')
+            .write('./static/img/draw.jpg', function (err) {
+                if (err) {
+                    console.log(err);
+                    reject(err);
+                }
+                resolve();
+            });
+    });
 }
